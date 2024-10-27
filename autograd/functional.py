aea15cb997529e591411bc23654811446e551e52
workofart <hanxiangp@gmail.com>
2024-10-26 22:25:45 -0700

    simplify functional

--- autograd/functional.py
+++ autograd/functional.py
@@ -12,10 +12,7 @@ def relu(x: Tensor) -> Tensor:
     
     def _backward():
         # dL/dx = dL/dy * dy/dx
-        if x.grad is None:
-            x.grad = out.grad * (x.data > 0)
-        else:
-            x.grad += out.grad * (x.data > 0)
+        x.grad += out.grad * (x.data > 0)
         
     out._backward = _backward
     return out
@@ -26,14 +23,11 @@ def sigmoid(x: Tensor) -> Tensor:
     Sigmoid activation function
     """
     
-    out = Tensor(1 / (1 + np.exp(-x.data)), prev=(x,))
+    out = Tensor(1 / (1 + np.exp(-x.data + 1e-7)), prev=(x,))
     
     def _backward():
         # d(sigmoid(x))/dx = sigmoid(x) * (1 - sigmoid(x))
-        if x.grad is None:
-            x.grad = out.grad * out.data * (1 - out.data)
-        else:
-            x.grad += out.grad * out.data * (1 - out.data)
+        x.grad += out.grad * out.data * (1 - out.data)
     
     out._backward = _backward
     return out
@@ -48,9 +42,10 @@ def binary_cross_entropy(y_pred: Tensor, y_true: Union[Tensor, np.ndarray]) -> T
         raise ValueError("y_pred and y_true must have the same shape")
         
     y_true = np.array(y_true.data)
-    y_pred_prob = y_pred.data + 1e-7
-    # compute the loss
     # Clip probabilities to prevent log(0)
+    y_pred_prob = np.clip(y_pred.data, 1e-15, 1 - 1e-15)
+    
+    # compute the loss
     out = Tensor(
         data=-np.mean(y_true * np.log(y_pred_prob) + (1 - y_true) * np.log(1 - y_pred_prob)),
         prev=(y_pred,), # this is very important to connect the loss tensor with the y_pred tensor
@@ -60,10 +55,7 @@ def binary_cross_entropy(y_pred: Tensor, y_true: Union[Tensor, np.ndarray]) -> T
         # dL/dpred = -(y/p - (1-y)/(1-p))
         print(f"y_true shape: {y_true.shape}")
         print(f"y_pred shape: {y_pred.data.shape}")
-        if y_pred.grad is None:
-            y_pred.grad = -(y_true / y_pred_prob - (1 - y_true) / (1 - y_pred_prob)) / len(y_pred_prob)
-        else:
-            y_pred.grad += -(y_true / y_pred_prob - (1 - y_true) / (1 - y_pred_prob)) / len(y_pred_prob)
+        y_pred.grad += -(y_true / y_pred_prob - (1 - y_true) / (1 - y_pred_prob)) / len(y_pred_prob)
     
     out._backward = _backward
     return out
